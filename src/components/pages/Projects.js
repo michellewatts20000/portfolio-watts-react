2f2476247863cd2759fd1abc0005775878d87713
Your NameMichelle <watts.e.michelle@gmail.com>
2021-07-14 19:54:02 +1000

    fixing up page structure

---
+++ src/components/pages/Projects.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default function Blog() {
+  return (
+    <div>
+      <h1>Project 1</h1>
+      <p>
+        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
+        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
+        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
+        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc.
+      </p>
+      <h1>Project 2</h1>
+      <p>
+        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
+        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
+        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
+        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc.
+      </p>
+      <h1>Project 3</h1>
+      <p>
+        Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula.
+        Curabitur pellentesque turpis sit amet eros iaculis, a mollis arcu
+        dictum. Ut vel ante eget massa ornare placerat. Etiam nisl orci, finibus
+        sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum nunc.
+      </p>
+    </div>
+  );
+}
